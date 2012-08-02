10785f145e8baa7fc127112a4fd8ddc302fbc84b
lyd-soton <yl20e11@soton.ac.uk>
2012-08-02 19:58:19 +0100

    OAuth twitter

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'VDKcz50UBWOWcE2VZQYFw');
+define('CONSUMER_SECRET', 'nFIgFEhc64Ub3nG7RUyosccCPl0IlkZ1nVVvkwndwKU');
+define('OAUTH_CALLBACK', 'http://localhost/callback.php');
